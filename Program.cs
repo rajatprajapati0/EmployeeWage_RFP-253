65da199bc893c92cf1d1546e1025fff740a542c9
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-28 13:00:58 +0530

    added : UC1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Transactions;
 
 namespace Employee_Wage
 {
     internal class Program
     {
         static void Main(string[] args)
+        { 
+            Console.WriteLine("Welcome to Employee Wage Computation");
+
+            //Check Employee is Present or Absent-Use((RANDOM)) for AttendanceCheck
+        
+            Console.WriteLine("enter your name");
+            string name = Console.ReadLine();
+
+           
+
+            Console.WriteLine(name);
+            Attendance();
+
+
+        }
+        static void Attendance()
         {
-            Console.WriteLine("Hello World!");
+            
+            Random rnd = new Random();
+            int n = rnd.Next(0, 2);
+
+            if (n == 0)
+            {
+                Console.WriteLine(" Employee is Absent");
+            }
+            else
+            {
+                Console.WriteLine(" Employee is Present");
+            }
         }
     }
 }
