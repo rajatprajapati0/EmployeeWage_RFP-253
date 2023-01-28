7840ae5b9d127d960ba05d98d71e5ce3de592f68
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-29 00:12:35 +0530

    Added :UC6

--- Program.cs
+++ Program.cs
@@ -7,6 +7,68 @@ namespace Employee_Wage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
+            Attendance();
         }
+        static void Attendance()
+        {
+            int perHour_wage = 20;
+            int fullDay_hour = 8;
+            int halfDay_hour = 4;
+            int halfday = 0;
+            int fullday = 0;
+            int wage = 0;
+            Random rnd = new Random();
+            int monthlyWage = 0;
+            int day = 0;
+            int count = 0;
+            int absent = 0;
+            int totalhour = 0;
+            while (day != 20 && count != 30 && totalhour!=100 )
+
+            {
+                int n = rnd.Next(0, 3);
+                switch (n)
+
+                {
+                    case 0:
+                        absent++;
+                        count++;
+                        break;
+
+                    case 1:
+
+                        wage = halfDay_hour * perHour_wage;
+                        monthlyWage = wage + monthlyWage;
+                        count++;
+                        halfday++;
+                        day++;
+                        totalhour += halfDay_hour;
+                        
+                        break;
+                    case 2:
+                        wage = fullDay_hour * perHour_wage;
+                        monthlyWage = wage + monthlyWage;
+                        count++;
+                        day++;
+                        fullday++;
+                        totalhour += fullDay_hour;
+
+                        break;
+                }
+            }
+            Console.WriteLine("monthly wage                  :" + monthlyWage);
+            Console.WriteLine("prasent days                  :" + day);
+            Console.WriteLine("total half day taken          :" + halfday);
+            Console.WriteLine("total full day taken          :" + fullday);
+            Console.WriteLine("total absent in a month       :" + absent);
+            Console.WriteLine("total worked hour in a month  :" + totalhour);
+            Console.ReadLine();
+         
+
+        }
+
+
+
+
     }
 }
